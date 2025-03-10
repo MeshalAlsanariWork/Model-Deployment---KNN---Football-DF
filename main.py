a876811727e097c00a2d430e38ddcdcf2270631a
MeshalAlsanariWork <m.s.alsanari@gmail.com>
2025-03-10 22:20:14 +0300

    txt 6

--- main.py
+++ main.py
@@ -4,3 +4,39 @@ import joblib
 import numpy as np
 
 app = FastAPI()
+
+model = joblib.load('knn_model.joblib')
+scaler = joblib.load('scaler.joblib')
+
+class InputFeatures(BaseModel):
+    minutes_played: int
+    award: int
+    age: int
+    appearance: int
+    highest_value: int
+
+
+def preprocessing(input_features: InputFeatures):
+    dict_f = {
+        'minutes_played': input_features.minutes_played,
+        'award': input_features.award,
+        'age': input_features.age,
+        'appearance': input_features.appearance,
+        'highest_value': input_features.highest_value
+    }
+    return dict_f
+
+
+@app.post("/predict")
+def predict(data: InputFeatures):
+    input_data = preprocessing(data)
+    input_array = np.array([list(input_data.values())]).reshape(1, -1)
+
+    # Transform input data using the scaler
+    input_scaled = scaler.transform(input_array)
+
+    # Make a prediction
+    prediction = model.predict(input_scaled)
+
+    # Return the prediction as a string instead of converting to int
+    return {"prediction": prediction[0]}  # No `int()` conversion needed
